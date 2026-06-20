db058993614a8edb8b3e246cfac4cedf2f1eb946
MTKIN <silmotoki@gmail.com>
2026-06-20 17:25:51 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nyuta-estimate-v2';
+const CACHE = 'nyuta-estimate-v3';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
